07220dbd4e4a722446d3011dff017a0474acbf2a
Alon Krymgand <me@alon.kr>
2024-08-30 00:41:06 +0300

    Added String() to MOV instructions

--- mov.go
+++ mov.go
@@ -1,3 +1,8 @@
+// An implementation of the MOVZ, MOVK and MOVN instructions.
+// Those are all of the explicit MOV instruction variants of an immediate to a
+// general purpose register in AArch64. The "MOV" instruction itself is an alias
+// to MOVZ with a shift of 0, and (when possible) a MOVN instruction with a high
+// immediate value that can be encoded using 16 bits and shifts.
 package aarch64
 
 import "fmt"
@@ -19,23 +24,61 @@ func (m MovShift) String() string {
 	return fmt.Sprintf("LSL #%d", m*16)
 }
 
+func movBuildRaw(magic uint32, dest GPRegister, imm Immediate16, shift MovShift) uint32 {
+	return (magic << 23) | (shift.Binary() << 21) | (imm.Binary() << 5) | (dest.Binary())
+}
+
+func movString(name string, dest GPRegister, imm Immediate16, shift MovShift) string {
+	s := fmt.Sprintf("%s %s, %s", name, dest, imm)
+	if shift != 0 {
+		s += fmt.Sprintf(", %s", shift)
+	}
+	return s
+}
+
+func movExtractDetails(m uint32) (uint32, GPRegister, Immediate16, MovShift) {
+	magic := m >> 23
+	shift := MovShift((m >> 21) & 0b11)
+	imm := Immediate16((m >> 5) & 0xFFFF)
+	dest := GPRegister(m & 0b11111)
+	return magic, dest, imm, shift
+}
+
+// MARK: MOVZ
+
 type movz uint32
 
 func MOVZ(dest GPRegister, imm Immediate16, shift MovShift) (movz, error) {
-	inst := (0b110100101 << 23) | (shift.Binary() << 21) | (imm.Binary() << 5) | (dest.Binary())
-	return movz(inst), nil
+	return movz(movBuildRaw(0b110100101, dest, imm, shift)), nil
 }
+
+func (m movz) String() string {
+	_, dest, imm, shift := movExtractDetails(uint32(m))
+	return movString("MOVZ", dest, imm, shift)
+}
+
+// MARK: MOVK
 
 type movk uint32
 
 func MOVK(dest GPRegister, imm Immediate16, shift MovShift) (movk, error) {
-	inst := (0b111100101 << 23) | (shift.Binary() << 21) | (imm.Binary() << 5) | (dest.Binary())
-	return movk(inst), nil
+	return movk(movBuildRaw(0b111100101, dest, imm, shift)), nil
 }
+
+func (m movk) String() string {
+	_, dest, imm, shift := movExtractDetails(uint32(m))
+	return movString("MOVK", dest, imm, shift)
+}
+
+// MARK: MOVN
 
 type movn uint32
 
 func MOVN(dest GPRegister, imm Immediate16, shift MovShift) (movn, error) {
-	inst := (0b100100101 << 23) | (shift.Binary() << 21) | (imm.Binary() << 5) | (dest.Binary())
-	return movn(inst), nil
+	return movn(movBuildRaw(0b100100101, dest, imm, shift)), nil
+}
+
+func (m movn) String() string {
+	_, dest, imm, shift := movExtractDetails(uint32(m))
+	return movString("MOVN", dest, imm, shift)
 }
